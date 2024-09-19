4b6e88c1deffea661d7f0a192d92974de74f89ca
Mitch Chanza <mitch.chanza@gmail.com>
2024-09-19 10:27:51 +0200

    update page.tsx

--- app/universe/page.tsx
+++ app/universe/page.tsx
@@ -1,13 +1,18 @@
 'use client';
 import { CardsHolder, CountdownComponent, QuizCards } from "@opherlabs/components";
 import React from "react";
+import { useSession } from "next-auth/react";
 import { files } from "./quizdata";
 export default function Home() {
   const target = new Date("September 26, 2024 20:00:00").getTime();
   const now = new Date().getTime();
+  const {data: session, status: loading} = useSession();
   const timeLeft = target - now;
   return (
     <div>
+      <pre>
+        {JSON.stringify({session, loading}, null, 2)}
+      </pre>
           { timeLeft<=0?<>
             <CardsHolder>
                 <QuizCards url="universe" files={files} />
